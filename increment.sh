e59ba7c65372b7bdfb7a0e1b4534a960970f6bed
expelme <96442085+expelme@users.noreply.github.com>
2021-12-20 22:38:30 +0300

    Add files via upload

---
+++ increment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "# My ПСЖ my rules
+<h3 align=\"center\">На данный момент Аркадий упомянул отчисление</h3>" > index.md
+
+echo "<h2 align=\"center\">" $1 " раз</h2>" >> index.md
+echo "<p align=\"center\"><img src=\"./psj.jpeg\" width=\"500px\"></p>" >> index.md
+
