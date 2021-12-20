32b17f94b1d52c7d700b2db953f23e68a0152343
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2021-12-20 22:49:31 +0300

    Changes in increment.sh

--- increment.sh
+++ increment.sh
@@ -6,3 +6,6 @@ echo "# My ПСЖ my rules
 echo "<h2 align=\"center\">" $1 " раз</h2>" >> index.md
 echo "<p align=\"center\"><img src=\"./psj.jpeg\" width=\"500px\"></p>" >> index.md
 
+git add .
+git commit -m "$1"
+git push
